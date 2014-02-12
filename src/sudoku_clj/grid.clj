720e7727047207d13cd59836bf45457d8498a3b7
Rickard Sundin <rickard@snart.nu>
2014-02-12 19:18:33 +0100

    Add function to get any row from a grid

--- src/sudoku_clj/grid.clj
+++ src/sudoku_clj/grid.clj
@@ -1,3 +1,9 @@
 (ns sudoku-clj.grid)
 
 (def empty-grid (vec (repeat 81 \_)))
+
+(defn row
+  "Get a row from sudoku grid"
+  [grid rowidx]
+  (let [startidx (* rowidx 9)]
+  (subvec grid startidx (+ startidx 9))))
--- test/sudoku_clj/grid_test.clj
+++ test/sudoku_clj/grid_test.clj
@@ -2,6 +2,20 @@
   (:require [midje.sweet :refer [facts fact]]
             [sudoku-clj.grid :refer :all]))
 
+(def puzzle
+      [8 \_  2 \_ \_  9 \_ \_ 1
+      \_  3 \_  1 \_ \_ \_  7 \_
+      \_ \_  6 \_  2 \_  9 \_  3
+       9 \_ \_  6 \_  5 \_  4 \_
+      \_ \_  4 \_ \_ \_  5 \_ \_
+      \_  5 \_  4 \_  7 \_ \_  8
+       2 \_  8 \_  3 \_  7 \_ \_
+      \_  6 \_ \_ \_  1 \_  2 \_
+       1 \_ \_  8 \_ \_  3 \_  9 ])
+
 (facts "about Sudoku grid"
   (fact (count empty-grid) => 81)
+  (fact (row puzzle 0) => [8 \_  2 \_ \_  9 \_ \_ 1])
+  (fact (row puzzle 8) => [1 \_ \_  8 \_ \_  3 \_  9])
 )
+
